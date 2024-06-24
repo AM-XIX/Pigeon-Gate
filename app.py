f5cf0a56b581b3c70ba39858c66b662308133259
LouisePrd <louise.peridy22@gmail.com>
2024-06-24 21:16:16 +0200

    add login and register

--- app.py
+++ app.py
@@ -12,15 +12,48 @@ mydb = mysql.connector.connect(
     database="pigeon-gate",
 )
 
+pigeons = [];
+
 mycursor = mydb.cursor()
 
 @app.route("/", methods=['GET', 'POST'])
 def welcome():
     if mydb.is_connected():
         mycursor.execute("SELECT * FROM Pigeon")
-        myresult = mycursor.fetchall()
-        print(myresult)
-    return render_template("welcome.html", pigeons=myresult);
+        testLogin = mycursor.fetchall()
+    return render_template("welcome.html", pigeons=testLogin);
+
+@app.route("/login")
+def login():
+    return render_template("login.html");
+
+@app.route("/register")
+def register():
+    return render_template("register.html");
+
+@app.route("/register_user", methods=['POST'])
+def register_user():
+    if request.method == 'POST':
+        pseudo = request.form['pseudo']
+        password = request.form['password']
+        mycursor.execute("INSERT INTO User (pseudo, password) VALUES (%s, %s)", (pseudo, password))
+        mydb.commit()
+        return render_template("welcome.html");
+
+@app.route("/login_user", methods=['POST'])
+def login_user():
+    if request.method == 'POST':
+        pseudo = request.form['pseudo']
+        password = request.form['password']
+        mycursor.execute("SELECT * FROM User WHERE pseudo = %s AND password = %s", (pseudo, password))
+        testLogin = mycursor.fetchall()
+        mycursor.execute("SELECT * FROM Pigeon")
+        resultPigeon = mycursor.fetchall()
+        if testLogin:
+            return render_template("welcome.html", pigeons=resultPigeon);
+        else:
+            return render_template("login.html");
+
 
 
 if __name__ == "__main__":
