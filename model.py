50bd251e2d046fafb71473862fbc4eab49c40c72
LouisePrd <louise.peridy22@gmail.com>
2024-06-25 20:04:41 +0200

    fix duplicate pigeon lists

--- model.py
+++ model.py
@@ -9,29 +9,7 @@ mydb = mysql.connector.connect(
     database="pigeon-gate",
 )
 
-pigeons = []
-
-def getAllPigeons():
-    mycursor = mydb.cursor()
-    mycursor.execute("SELECT * FROM Pigeon")
-    allPigeons = mycursor.fetchall()
-    return dataToPigeon(allPigeons)
-
-def dataToPigeon(datas):
-    for pigeon in datas:
-        pigeon = {
-            "idPigeon": pigeon[0],
-            "prenomPigeon": pigeon[1],
-            "color": pigeon[2],
-            "rateWalk": pigeon[3],
-            "rateVibe": pigeon[4],
-            "rateOriginality": pigeon[5],
-            "place": pigeon[6],
-            "urlPhoto": pigeon[7],
-            "idUser": pigeon[7]
-        }    
-        pigeons.append(pigeon)
-    return pigeons
+## User
 
 def getUserbyPseudo(pseudo):
     mycursor = mydb.cursor()
@@ -45,11 +23,6 @@ def getUserbyPseudo(pseudo):
         "sommePigeons": sumAllPigeonsByUser(userData[0][0])
     }
     return user
-
-def addPigeon(prenomPigeon, color, rateWalk, rateVibe, rateOriginality, place, urlPhoto, idUser):
-    mycursor = mydb.cursor()
-    mycursor.execute("INSERT INTO Pigeon (prenomPigeon, color, rateWalk, rateVibe, rateOriginality, place, urlPhoto, idUser) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (prenomPigeon, color, rateWalk, rateVibe, rateOriginality, place, urlPhoto, idUser))
-    mydb.commit()
 
 def checkLogin(pseudo, password):
     mycursor = mydb.cursor()
@@ -67,8 +40,56 @@ def newUser(pseudo, password, typeProfilePicture):
     mydb.commit()
     return True
 
+def changeBio(idUser, newBio):
+    mycursor = mydb.cursor()
+    mycursor.execute("UPDATE User SET bio = %s WHERE idUser = %s", (newBio, idUser))
+    mydb.commit()
+
 def sumAllPigeonsByUser(idUser):
     mycursor = mydb.cursor()
     mycursor.execute("SELECT COUNT(*) FROM Pigeon WHERE idUser = %s", (idUser,))
     sumPigeons = mycursor.fetchone()
+    if not sumPigeons:
+        return 0
     return sumPigeons[0]
+
+
+## Pigeon
+
+def getAllPigeons():
+    mycursor = mydb.cursor()
+    mycursor.execute("SELECT * FROM Pigeon")
+    allPigeons = mycursor.fetchall()
+    return dataToPigeon(allPigeons)
+
+def dataToPigeon(datas):
+    pigeons = []
+    for pigeon in datas:
+        pigeon = {
+            "idPigeon": pigeon[0],
+            "prenomPigeon": pigeon[1],
+            "color": pigeon[2],
+            "rateWalk": pigeon[3],
+            "rateVibe": pigeon[4],
+            "rateOriginality": pigeon[5],
+            "place": pigeon[6],
+            "urlPhoto": pigeon[7],
+            "idUser": pigeon[8]
+        }    
+        pigeons.append(pigeon)
+    return pigeons
+
+
+def addPigeon(prenomPigeon, color, rateWalk, rateVibe, rateOriginality, place, urlPhoto, idUser):
+    mycursor = mydb.cursor()
+    mycursor.execute("INSERT INTO Pigeon (prenomPigeon, color, rateWalk, rateVibe, rateOriginality, place, urlPhoto, idUser) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (prenomPigeon, color, rateWalk, rateVibe, rateOriginality, place, urlPhoto, idUser))
+    mydb.commit()
+
+def getLastPigeonsByUser(idUser, page):
+    limit = 4
+    offset = page * limit
+    mycursor = mydb.cursor()
+    query = "SELECT * FROM Pigeon INNER JOIN User ON Pigeon.idUser = User.idUser WHERE Pigeon.idUser = %s ORDER BY Pigeon.idPigeon DESC LIMIT %s OFFSET %s"
+    mycursor.execute(query, (idUser, limit, offset))
+    lastPigeons = mycursor.fetchall()
+    return dataToPigeon(lastPigeons)
--- routeur.py
+++ routeur.py
@@ -4,13 +4,13 @@ import model
 app = Flask(__name__)
 
 app.config.from_object('config.Config')
-pigeons = model.getAllPigeons()
 
 
 @app.route("/", methods=['GET', 'POST'])
 def welcome():
     if model.mydb.is_connected():
         session.clear()
+        pigeons = model.getAllPigeons()
         return render_template("welcome.html", pigeons=pigeons);
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -25,7 +25,9 @@ def register():
             return render_template("register.html", messageErrorRegister=messageErrorRegister);
         else:
             session['pseudo'] = pseudo
+            session['idUser'] = model.getUserbyPseudo(pseudo)['idUser']
             model.newUser(pseudo, password, typeProfilePicture)
+            pigeons = model.getAllPigeons()
             return render_template("welcome.html", pigeons=pigeons, pseudo=pseudo);
     else:
         return render_template("register.html");
@@ -36,7 +38,9 @@ def login():
         pseudo = request.form['pseudo']
         password = request.form['password']
         if model.checkLogin(pseudo, password):
+            session['idUser'] = model.getUserbyPseudo(pseudo)['idUser']
             session['pseudo'] = pseudo
+            pigeons = model.getAllPigeons()
             return render_template("welcome.html", pigeons=pigeons, pseudo=pseudo);
         else:
             messageErrorLogin = "Pseudo ou mot de passe incorrect"
@@ -51,6 +55,7 @@ def add_pigeon():
         return render_template("login.html");
     if request.method == 'POST' and 'pseudo' in session:
         model.addPigeon(request.form['prenom'], request.form['couleur'], request.form['noteWalk'], request.form['notevibe'], request.form['noteoriginalite'], request.form['place'], request.form['urlPhoto'], session['pseudo'])
+        pigeons = model.getAllPigeons()
         return render_template("welcome.html", pigeons=pigeons);
     else:
         return render_template("new_pigeon.html");
@@ -58,14 +63,18 @@ def add_pigeon():
 @app.route("/logout", methods=['POST', 'GET'])
 def logout():
     session.clear()
+    pigeons = model.getAllPigeons()
     return render_template("welcome.html", pigeons=pigeons);
 
 @app.route("/profil", methods=['POST', 'GET'])
 def profil():
     if 'pseudo' not in session:
-        return render_template("login.html");
+        return render_template("login.html") 
     user = model.getUserbyPseudo(session['pseudo'])
-    return render_template("profile.html", user=user);
+    page = request.args.get('page', default=0, type=int)
+    lastPigeons = model.getLastPigeonsByUser(user['idUser'], page)
+    return render_template("profile.html", user=user, lastPigeons=lastPigeons, page=page)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
